f4b4b3385c916a14d0fc1dc69b23d883b3485b43
Juliane Prado <112557540+JulianePrado@users.noreply.github.com>
2023-03-01 11:51:01 -0300

    Update and rename PageController.js to HomeController.js

--- ProjetoFinal/backend/src/controller/HomeController.js
+++ ProjetoFinal/backend/src/controller/HomeController.js
@@ -1,15 +1,15 @@
 import { verificarToken } from "../middleware/authorization.js"
-import Page from "../DAO/Page.js"
+import Home from "../DAO/Home.js"
 
-// page irá ser home 
-// page controller vira homecontroller 
+// home irá ser home 
+// home controller vira homecontroller 
 
 
 // O método estático "rotas" adiciona duas rotas ao objeto "app"
-export default class PageController {
+export default class HomeController {
     static rotas(app) {
-        app.get('/paginas/:id', PageController.listar)
-        app.patch('/paginas/:id', verificarToken, PageController.atualizar)
+        app.get('/paginas/:id', HomeController.listar)
+        app.patch('/paginas/:id', verificarToken, HomeController.atualizar)
     } // A primeira rota é um endpoint HTTP GET que escuta em "/paginas/:id"
 
     // A segunda rota é um endpoint HTTP PATCH que escuta em "/paginas/:id"
@@ -22,15 +22,15 @@ export default class PageController {
         // Extrai o parâmetro ":id" da requisição usando a desestruturação de objetos
         const {id} = req.params
         // Usa o método "findByProperty" do modelo "Page" para buscar uma página com o mesmo valor de id
-        const page = await Page.findByProperty('id', id)
+        const home = await Home.findByProperty('id', id)
 
      // Se não houver uma página com esse id, retorna uma mensagem de erro (status 404)
-        if (!page) { 
+        if (!home) { 
             return res.status(404).send({
-                message: 'Página não encontrada'
-            })
+                message: 'Página não encontrada' 
+            }) // Se a página for encontrada, retorna uma mensagem de sucesso (status 200) e a página encontrada.
         }
-         // Se a página for encontrada, retorna uma mensagem de sucesso (status 200) e a página encontrada.
+      
         res.status(200).send({
             message: 'Sucesso ao buscar página',
             data: page
@@ -40,22 +40,22 @@ export default class PageController {
     static async atualizar(req, res) {
         const {id} = req.params
         const {title, text} = req.body
-        const page = await Page.findByProperty('id', id)
-        if (!page) {
+        const home = await Home.findByProperty('id', id)
+        if (!home) {
             return res.status(404).send({
                 message: 'Página não encontrada'
             })
         }
         if (title) {
-            page.title = title
+            home.title = title
         }
         if (text) {
-            page.text = text
+            home.text = text
         }
-        await page.save()
+        await home.save()
         res.status(200).send({
             message: 'Sucesso ao alterar dados da página',
-            data: page
+            data: home
         })
     }
 }
